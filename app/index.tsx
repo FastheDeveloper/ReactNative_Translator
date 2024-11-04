3143d4d97c0c3f1230965ce5e78ae171673ec448
Farouq Seriki <farouq@basaltx.io>
2024-11-04 16:37:06 +0100

    initial

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,12 @@
-import { Stack, Link } from 'expo-router';
+import { Stack } from 'expo-router';
+import { Text } from 'react-native';
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+    <Text className='text-xl font-bold color-red-500 m-10'>Hello my work</Text>
     </>
   );
 }
