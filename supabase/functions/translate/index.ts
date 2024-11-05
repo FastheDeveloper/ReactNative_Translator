f32a06d2e04f4c1510e447b14035f3f1b8671ef5
Farouq Seriki <farouq@basaltx.io>
2024-11-05 21:58:57 +0100

    CORS

--- supabase/functions/translate/index.ts
+++ supabase/functions/translate/index.ts
@@ -1,10 +1,16 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import OpenAI from "npm:openai";
+import { corsHeaders } from '../_shared/cors.ts'
 const openai = new OpenAI();
  
 console.log("Hello from Functions!")
 
 Deno.serve(async (req) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+
   const { input,from, to } = await req.json()
 
   const completion = await openai.chat.completions.create({
@@ -23,7 +29,7 @@ console.log(completion.choices[0].message);
 
   return new Response(
     JSON.stringify(completion.choices[0].message),
-    { headers: { "Content-Type": "application/json" } },
+    { headers: { ...corsHeaders,"Content-Type": "application/json" } },
   )
 })
 
